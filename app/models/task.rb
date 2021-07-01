86c99e087308230f88ffc0e32f573c9c923439b0
realvint <victor.malukhin@gmail.com>
2021-07-01 18:26:24 +0300

    basic functionality completed

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,27 @@ class Task < ApplicationRecord
   validates :status, inclusion: { in: %w[not-started in-progress complete] }
 
   STATUS_OPTIONS = [['Not started', 'not-started'], ['In progress', 'in-progress'], ['Complete', 'complete']].freeze
+
+  def badge_color
+    case status
+    when 'not-started'
+      'secondary'
+    when 'in-progress'
+      'info'
+    when 'complete'
+      'success'
+    end
+  end
+
+  def complete?
+    status == 'complete'
+  end
+
+  def in_progress?
+    status == 'in-progress'
+  end
+
+  def not_started?
+    status == 'not-started'
+  end
 end
